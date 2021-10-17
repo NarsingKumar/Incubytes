2cacbb9bf46266c6702fe71d936050f0ccadecb9
Narsing Kumar <narsingkumar7587@gmail.com>
2021-10-17 15:39:43 +0530

    string_with_two_number_should_return_number_as_sum

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -5,8 +5,13 @@ class StringCalculator {
     public int add(String input) {
     	if(input == null || input.isEmpty()) {
     		return 0;
-    	} 
+    	} else if(input.length() == 1) {
     		return Integer.parseInt(input);
+    	}  
+    	else {
+    		String[] numbers = input.split(",");
+    		return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+    	}
     	    
     }
 
